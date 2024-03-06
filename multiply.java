28f733ca340c282c63841e88bb89b3df06ba8c31
Adiba <adibashaikhani59@Gmail.com>
2024-03-06 17:39:56 +0530

    Commit Complete

--- multiply.java
+++ multiply.java
@@ -1,9 +1,9 @@
-public class addition {
+public class multiply {
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
         int c = 3;
-        System.out.println(a + b + c);
+        System.out.println(a * b * c);
 
     }
 }
